f366a5f8b95ffe8c883923c4cc839e829dd24171
tigarto <helpu1409@gmail.com>
2025-10-09 22:24:35 -0500

    actualizando repositorio

--- lab2/README.md
+++ lab2/README.md
@@ -1,7 +1,10 @@
 # Laboratorio 2
 
-Antes de empezar la practica analice el siguiente [[pdf]](bosquejo_wish-shell.pdf) el cual resume 
-Para desarrollar la practica. Luego, de un vistazo a cualquiera de las siguientes dos paginas para que tenga una idea global de como se implementa un shell en C.
+Antes de empezar la practica analice el siguiente [[pdf]](bosquejo_wish-shell.pdf) el cual resume como desarrollar la practica. 
+
+Como apoyo para facilitar la comprensión del pdf se muestra un versión resumida del shell en python ([wish.py](wish.py)). Este programa permite ejecutar comandos internos (exit, cd, path) y externos, utilizando funcionalidades como os.fork() y os.execvp() para manejar procesos.
+
+Luego, de un vistazo a cualquiera de las siguientes dos paginas para que tenga una idea global de como se implementa un shell en C.
 *  **Tutorial - Write a Shell in C** [[link]](https://brennan.io/2015/01/16/write-a-shell-in-c/)
 *  **Building Simple Shell in C** [[link]](https://dev.to/hussein_hazimeh_1e1bddf94/simple-shell-in-c-3l3i)
 
---
+++ lab2/wish.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+# wish.py: A simple shell implementation in Python
+
+"""
+- No esta implementado el modo batch.
+- No esta implementado el redireccionamiento.
+- No esta implementada la ejecucion de comandos en paralelo.
+"""
+
+import os
+import sys
+
+internal_commands = ["exit", "cd", "path"]
+path = ["/bin", "/usr/bin", "."]
+command_exist = False
+
+def execute_command(command_list):
+    global command_exist
+    if command_list[0] in internal_commands:
+        # Internal commands: exit, cd, path
+        if command_list[0] == "exit":
+            # exit command
+            print("Exiting wish shell.")
+            sys.exit(0)
+        elif command_list[0] == "cd":
+            # cd command
+            if len(command_list) != 2:
+                print("cd: Missing argument")
+            else:
+                os.chdir(command_list[1])            
+        elif command_list[0] == "path":
+            # path command
+            path.clear()
+            path.extend(command_list[1:])
+            command_exist = False
+            print(f"Updated path: {path}")
+    else:
+        # External commands        
+        for dir in path:                    
+            command_path = dir + "/" + command_list[0]                                    
+            if os.access(command_path, os.X_OK):
+                command_list[0] = command_path
+                command_exist = True
+                break
+        if command_exist == False:
+            print(f"{command_list[0]}: Command not found")
+        else:
+            # Fork and execute the command  
+            pid = os.fork()                              
+            if pid == 0:
+                # Child process
+                os.execvp(command_list[0], command_list)            
+            else:
+                # Parent process
+                os.wait()
+        
+def main():
+    if len(sys.argv) > 2:
+        print("Usage: wish [batch_file]")
+        sys.exit(1) 
+    elif len(sys.argv) == 2:
+        # Batch mode
+        print("Batch mode not implemented in this version.")
+        sys.exit(1)
+    else:
+        # Interactive mode
+        while True:
+            command = input("wish> ")
+            command_list = [param.strip() for param in command.split()]
+            # print(f"Command List: {command_list}")
+            execute_command(command_list)
+
+if __name__ == "__main__":
+    main()
